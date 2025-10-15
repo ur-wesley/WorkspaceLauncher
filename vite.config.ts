7f9dda3aa8e2b2dfab4831b179b1f5374817cd1f
Wesley Urban <wes.urb@gmail.com>
2025-10-15 18:31:05 +0200

    chore: update project configuration

--- vite.config.ts
+++ vite.config.ts
@@ -11,29 +11,27 @@ const __dirname = dirname(__filename);
 const host = process.env.TAURI_DEV_HOST;
 
 export default defineConfig({
- plugins: [solidPlugin(), UnocssPlugin()],
- clearScreen: false,
- // 2. tauri expects a fixed port, fail if that port is not available
- server: {
-  port: 1420,
-  strictPort: true,
-  host: host || false,
-  hmr: host
-   ? {
-      protocol: "ws",
-      host,
-      port: 1421,
-     }
-   : undefined,
-  watch: {
-   // 3. tell Vite to ignore watching `src-tauri`
-   ignored: ["**/src-tauri/**"],
+  plugins: [solidPlugin(), UnocssPlugin()],
+  clearScreen: false,
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+        protocol: "ws",
+        host,
+        port: 1421,
+      }
+      : undefined,
+    watch: {
+      ignored: ["**/src-tauri/**"],
+    },
   },
- },
 
- resolve: {
-  alias: {
-   "@": resolve(__dirname, "./src"),
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
   },
- },
 });
