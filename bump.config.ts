7ace8d883d12c64297ebd13bbfc9cc7add4a60b4
Wesley Urban <wes.urb@gmail.com>
2025-10-15 18:29:37 +0200

    refactor: simplify bump config for multi-file versioning

---
+++ bump.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "bumpp"
+
+export default defineConfig({
+  files: [
+    "package.json",
+    "src-tauri/Cargo.toml",
+    "src-tauri/tauri.conf.json"
+  ],
+  commit: true,
+  tag: true,
+  push: true,
+  all: true
+})
