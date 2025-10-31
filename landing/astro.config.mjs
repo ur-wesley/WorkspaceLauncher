95ae2e5e2b2308fb7a954a79db37490561620971
wesley <wes.urb@gmail.com>
2025-10-31 16:26:00 +0100

    Create landing page with Astro.js

---
+++ landing/astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import solidJs from "@astrojs/solid-js";
+import UnoCSS from "unocss/astro";
+
+// https://astro.build/config
+export default defineConfig({
+ integrations: [
+  solidJs(),
+  UnoCSS({
+   injectReset: true,
+  }),
+ ],
+});
