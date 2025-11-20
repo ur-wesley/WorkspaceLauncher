bc774ce31a38963450ae34c2f100d90d32b72377
Wesley Urban <wes.urb@gmail.com>
2025-11-20 17:40:48 +0100

    chore: formatting

--- bump.config.ts
+++ bump.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "bumpp";
 
 export default defineConfig({
- files: [
-  "package.json",
-  "landing/package.json",
-  "src-tauri/Cargo.toml",
-  "src-tauri/tauri.conf.json",
- ],
+  files: [
+    "package.json",
+    "landing/package.json",
+    "src-tauri/Cargo.toml",
+    "src-tauri/tauri.conf.json",
+  ],
 });
